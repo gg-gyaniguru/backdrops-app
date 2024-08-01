df8b4f95836e885eb462dd04f2d626c9ad564b74
gg-gyaniguru <gg.gyaniguru@gmail.com>
2024-08-01 15:29:12 +0530

    fix bug

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // baseURL: 'https://backdrops-api.onrender.com/api',
-    baseURL: 'http://localhost:9060/api',
+    baseURL: 'https://backdrops-api.onrender.com/api',
+    // baseURL: 'http://localhost:9060/api',
 });
 
 export default instance;
