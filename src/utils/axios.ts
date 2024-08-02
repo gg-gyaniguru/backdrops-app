59ca84bbe65252aff57e185213512f5e298f9605
gg-gyaniguru <gg.gyaniguru@gmail.com>
2024-08-02 20:02:38 +0530

    bug fix

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -6,8 +6,8 @@ const accessToken = getKey('accessToken');
 const {VITE_SECRET_KEY, VITE_API_URL} = import.meta.env;
 
 const instance = axios.create({
-    // baseURL: VITE_API_URL,
-    baseURL: 'http://localhost:9060/api',
+    baseURL: VITE_API_URL,
+    // baseURL: 'http://localhost:9060/api',
     headers: {
         'Secret': VITE_SECRET_KEY,
         'Authorization': `Bearer ${accessToken}`
